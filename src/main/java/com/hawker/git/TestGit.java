c83fd28a73b0f0f2496ecd4a04ae99c3bf9c805c
Hawker <Hawker@qq.com>
2022-09-07 11:31:11 +0800

    first commit

---
+++ src/main/java/com/hawker/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.hawker.git;
+
+/**
+ * @author Hawker
+ * @create 2022-02-12 17:05
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
